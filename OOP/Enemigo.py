44ae76e1574b6c7229f1c401f3c57c52c1acb78d
gaelguevara8 <gaelguevara030@gmail.com>
2026-02-27 15:43:08 -0800

    Modificando de mis repositorios

--- OOP/Enemigo.py
+++ OOP/Enemigo.py
@@ -3,9 +3,9 @@ class Enemigo:
     puntos_energia: int = 10
     ataque = 1
 
-    def __init__(self, tipo_enemigo, punto_energia=10, ataque=1):
+    def __init__(self, tipo_enemigo, puntos_energia=10, ataque=1):
         self.__tipo_enemigo = tipo_enemigo
-        self.puntos_energia = punto_energia
+        self.puntos_energia = puntos_energia
         self.ataque = ataque 
 
     def get_tipo_enemigo(self):
@@ -19,3 +19,6 @@ class Enemigo:
 
     def atacar(self):
        print(f"{self.__tipo_enemigo} ataca con un {self.ataque} de daño!!")
+
+    def ataque_especial(self):
+       print("Enemigo no tiene ataque especial")
